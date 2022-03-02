8a7bac0da3a8b1ad3a83241161f9052e51044830
MesutRuzgar <mesutruzgar@gmail.com>
2022-03-02 18:18:32 +0300

    Add project files.

---
+++ DevFramework.Business.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DevFramework.Business.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DevFramework.Business.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("458d04c2-f2de-42ee-999f-cbb86841f1dd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DevFramework.Northwind.Core/DataAccess/IQueryableReposityory.cs
@@ -0,0 +1,14 @@
+﻿using DevFramework.Northwind.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Core.DataAccess
+{
+   public  interface IQueryableReposityory<T>where T:class,IEntity,new()
+    {
+        IQueryable<T> Table { get; }
+    }
+}
